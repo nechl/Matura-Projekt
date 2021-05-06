af0faa4b4268564d7144dce73c7553bad94834f1
nechl <45829474+nechl@users.noreply.github.com>
2021-05-06 19:47:03 +0200

    test for the new function

--- helping_f.py
+++ helping_f.py
@@ -5,7 +5,3 @@ class helping_functions:
     def printd(self, msg, lvl=1):
         if lvl <= self.debug_level:
             print(msg)
-
-DEBUG_LEVEL = 1
-x = helping_functions(DEBUG_LEVEL)
-x.printd("hi", 1)
---
+++ test.py
@@ -0,0 +1,10 @@
+from helping_f import helping_functions
+
+# Here you can add a certain debug level, so that you are able to edit the comments which get called, if printd is higher than DEBUG_LEVEL, it gets printed, else it will be skipped.
+DEBUG_LEVEL = 2
+
+printd = helping_functions(DEBUG_LEVEL).printd
+
+printd("This is a level 2 debug comment",2)
+printd("This is a level 1 debug comment",1)
+printd("This is a level 0 debug comment",0)
