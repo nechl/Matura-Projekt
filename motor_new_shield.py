ee1a5a2df4b1b5abb449eeb2705188d98b45192c
nechl <nicolas.eric.perruchoud@gmail.com>
2021-09-24 12:05:33 +0200

    change of motor new shield

--- motor_new_shield.py
+++ motor_new_shield.py
@@ -4,42 +4,46 @@ try:
 except ModuleNotFoundError as error:
     print(error)
 step = 21
-dir = 12
-class motor_shield():
-    def __init__(self, step, dir):
-        self.step = step
-        self.dir = dir
+dir1 = 12
 
+delay = 0.01
+class motor_shield():
+    def __init__(self, step, dir1):
+        self.step = step
+        self.dir1 = dir1
+	
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.step, GPIO.OUT)
+        GPIO.setup(self.dir1, GPIO.OUT)
     def setup(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.step, GPIO.OUT)
-        GPIO.setup(self.dir, GPIO.OUT)
+        GPIO.setup(self.dir1, GPIO.OUT)
 
     def DownStep(self):
-        GPIO.output(self.dir, GPIO.LOW)
+        GPIO.output(self.dir1, GPIO.LOW)
 
-        for i in range(100):
+        for i in range(500):
             GPIO.output(self.step, GPIO.LOW)
-            time.sleep(0.01)
+            time.sleep(delay)
             GPIO.output(self.step, GPIO.HIGH)
-            time.sleep(0.01)
+            time.sleep(delay)
 
     def UpStep(self):
-        GPIO.output(self.dir, GPIO.HIGH)
-        
-        for i in range(100):
+        GPIO.output(self.dir1, GPIO.HIGH)
+        for i in range(500):
             GPIO.output(self.step, GPIO.LOW)
-            time.sleep(0.01)
+            time.sleep(delay)
             GPIO.output(self.step, GPIO.HIGH)
-            time.sleep(0.01)
+            time.sleep(delay)
 
     def destroy(self):
-        GPIO.output(self.dir, GPIO.LOW)
+        GPIO.output(self.dir1, GPIO.LOW)
         GPIO.output(self.step, GPIO.LOW)
         GPIO.cleanup()
     
 if __name__ == "__main__":
-    motor_shield = motor_shield(step, dir)
+    motor_shield = motor_shield(step, dir1)
     while True:
         x = input("Up[1] or down[2]: ")
         motor_shield.UpStep() if x == "1" else motor_shield.DownStep()
