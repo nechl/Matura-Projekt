6fa091e5290903fdf4beb570759bdd9af7d156ec
nechl <45829474+nechl@users.noreply.github.com>
2021-11-16 09:49:56 +0100

    Update temperatur.py

--- website_backend/physical/temperatur.py
+++ website_backend/physical/temperatur.py
@@ -19,11 +19,9 @@ if __name__ == "__main__":
     
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.flush()
-    temp = 20.1
+    temp = 20.0
     while True:
-        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-        ser.flush()
-                
+               
         if ser.in_waiting > 0:
             temp = ser.readline().decode('utf-8').rstrip()
             print(str(temp))
